e2f70b58ba7d3e3e104152f5556620f70a963256
j2023 <j2023@192.168.11.2>
2024-10-09 08:59:25 +0900

    20241009

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -1,10 +1,13 @@
 package co.jp.chapter01;
 
 public class JavaHomework01 {
-
+	//int n = 0;
+	//　a　>　10の場合、１１を返す、そうでもない場合、０を返す
+	//==
+	//true false
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-         System.out.print("hello world");
+         
 	}
-
+	
 }
