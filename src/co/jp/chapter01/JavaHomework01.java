8e8fdedb77d454bf92e2f01802a22b7fd5f015e3
gendogo <whj.bnm@qq.com>
2024-10-08 20:35:06 +0800

    answer the questions

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -4,6 +4,12 @@ public class JavaHomework01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		/* 問題1：0
+		 * 問題2：aが10より大きければaに11を代入します。
+		 		小さければ、aに0を代入します。
+		 * 問題3：==
+		 * 問題4：true false
+		 */
 
 	}
 
