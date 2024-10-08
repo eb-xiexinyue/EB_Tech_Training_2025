781bdc92550ced8c4b85b362f2e34494545cd9d5
xie <xie@192.168.1.3>
2024-10-08 21:30:35 +0900

    20241008 コミット

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -2,9 +2,93 @@ package co.jp.chapter01;
 
 public class JavaChapter01 {
 
+	final static int fixedNum = 99;
+	
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
+		//変数＆定数
+		
+		int num = 20;
+		long longNum = 20000;
+		float floatNum = (float)20.00f;
+		char char1 = 'F';
+		boolean bl = true;
+		
+		int num1 = 25;
+		
+		num1 = (int)100.0f;
+		
+		final int fixedNum1 = 999;
+		
+//		System.out.println("type:int value:"+num);
+//		System.out.println("type:long value:"+longNum);
+//		System.out.println("type:float value:"+floatNum);
+//		System.out.println("type:char value:"+char1);
+//		System.out.println("type:boolean value:"+bl);
+//		
+//		System.out.println("type:int value:"+num1);
+//		
+//		System.out.println("type:int(constant) value:"+fixedNum);
+//		System.out.println("type:int(constant) value:"+fixedNum1);
+		
+		//演算子
+		int a = 20;
+		int b = 30;
+		
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
+		
+//		System.out.println(a);
+//		System.out.println(a++);
+//		System.out.println(a);
+//		
+//		System.out.println(a);
+//		System.out.println(++a);
+//		System.out.println(a);
+		
+//		System.out.println(b);
+//		System.out.println(b--);
+//		System.out.println(b);
+//		
+//		System.out.println(b);
+//		System.out.println(--b);
+//		System.out.println(b);
+		
+//		System.out.println("10==10 :"+(10==10));
+//		System.out.println("10==11 :"+(10==11));
+//		
+//		System.out.println("10!=10 :"+(10!=10));
+//		System.out.println("10!=11 :"+(10!=11));
+//		
+//		System.out.println("11>10 :"+(11>10));
+//		System.out.println("10>11 :"+(10>11));
+//		
+//		System.out.println("11>=10 :"+(11>=10));
+//		System.out.println("10>=11 :"+(10>=11));
+//		
+//		System.out.println("11<10 :"+(11<10));
+//		System.out.println("10<11 :"+(10<11));
+//		
+//		System.out.println("11<=10 :"+(11<=10));
+//		System.out.println("10<=11 :"+(10<=11));
+		
+//		System.out.println("10 < 11 || 10 == 11 :"+(10 < 11 || 10 == 11));
+//		System.out.println("10 < 11 && 10 == 11 :"+(10 < 11 && 10 == 11));
+//		
+//		System.out.println("10 > 11 || 10 == 11 :"+(10 > 11 || 10 == 11));
+//		System.out.println("10 < 11 && 11 == 11 :"+(10 < 11 && 11 == 11));
+//		
+//		System.out.println("!(10>11) :"+!(10>11));
+		
+		System.out.println("10 > 9 ? 1 : 0");
+		System.out.println(10 > 9 ? 1 : 0);
+		
+		System.out.println("10 > 11 ? 1 : 0");
+		System.out.println(10 > 11 ? 1 : 0);
+		}
 
 }
