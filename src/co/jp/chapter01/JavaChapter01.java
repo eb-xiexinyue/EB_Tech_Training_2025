d184f05568efe95044d8bac6f0a8733b152b25ff
DengGuan96 <515728277@qq.com>
2024-10-11 01:44:42 +0900

    chapter01

--- src/co/jp/chapter01/JavaChapter01.java
+++ src/co/jp/chapter01/JavaChapter01.java
@@ -4,7 +4,21 @@ public class JavaChapter01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		int num = 20;
+		long longNum = 20000;
+		float floatNum = 20.00f;
+		char char1 = 'F';
+		boolean b = true;
+		int num1 = 25;
+		num1 = (int)100.0f;
+		
+		
+		System.out.println("type:int value:" + num);
+		System.out.println("type:long value:" + longNum);
+		System.out.println("type:float value:" + floatNum);
+		System.out.println("type:char value:" + char1);
+		System.out.println("type:boolean value:" + b);
+		System.out.println("type:int value:" + num1);
 	}
 
 }
