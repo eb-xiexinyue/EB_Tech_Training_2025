c6fde222841fbd50f8499e4b6816840876ed182a
ojiwa <ojiwa@R9000P2023SHEN>
2024-10-11 17:11:25 +0900

    241010Home

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -4,6 +4,20 @@ public class JavaHomework01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問1
+		int i = 10;
+		int n = i % 5;
+		System.out.println("int:"+n);
+		//質問2　右の式を解釈してください: a = a > 10 ? 11 : 0;
+		//a > 10の場合、aに11を代入。a≤10の場合、aに0を代入 
+
+		// 質問3
+		//==:比較、=：代入
+		
+		//質問4：
+		//ブリアン（ boolean ）変数に設定可能の値は「true」「false」。
+		
+		
 
 	}
 
