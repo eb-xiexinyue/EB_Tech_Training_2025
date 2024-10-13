6ce09eca0ec68baf254eafe26d58b5e30120598b
chocoooolate <chocoooolate@localhost>
2024-10-13 11:24:54 +0900

    Chapter01の宿題

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -4,6 +4,19 @@ public class JavaHomework01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		// 質問1
+		int I = 10;
+		int n = I % 5;
+		System.out.println("結果:"+n);
+		
+		// 質問2
+		// a が 10 より大きければ 11 に設定し、そうでなければ 0 に設定する
+		
+		// 質問3
+		// ==
+		
+		// 質問4
+		// 設定可能の値は「true」「false」
 
 	}
 
