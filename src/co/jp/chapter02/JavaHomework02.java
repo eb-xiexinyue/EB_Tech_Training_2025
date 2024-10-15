55f5e01d3af561ca09b9fb396d952c733dce9f74
j2023 <j2023@192.168.1.29>
2024-10-15 20:16:10 +0900

    20241015

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -1,10 +1,15 @@
 package co.jp.chapter02;
 
 public class JavaHomework02 {
-
+//2行
+//equals
+//String.valueOf("1.0F")
+//3.0,"1.02.0"
+//a
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+            int a = 10 ;
+            System.out.println("a");
 	}
 
 }
