469be8f8c58254fcf8af385830eb8a8a0f7754d6
gendogo <whj.bnm@qq.com>
2024-10-15 21:13:20 +0800

    chapter2と3の回答です。

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -4,6 +4,14 @@ public class JavaHomework02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		/* 問題1：1
+		 * 問題2：2
+		 * 問題3：2
+		 * 問題4：3.0 1.02.0
+		 * 問題5：a
+		 */
+	
+
 
 	}
 
--- src/co/jp/chapter03/JavaHomework03.java
+++ src/co/jp/chapter03/JavaHomework03.java
@@ -4,6 +4,34 @@ public class JavaHomework03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問１
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		if (a >= 90) {
+			System.out.println("A");
+		} else if (a >= 80 && a < 90 ) {
+			System.out.println("B");
+		}
+		else if (a >= 70 && a < 80 ) {
+			System.out.println("C");
+		}
+		else if (a >= 60 && a < 70 ) {
+			System.out.println("D");
+		}else {
+			System.out.println("E");
+		}
+		
+		//質問2: 最も少ないコイン数は13です。
+		int total = 3340;
+		int cnt = 0;
+		int[] arr = {1,5,10,100,500};
+		for (int i = arr.length - 1, temp ; i >= 0 ; i--) {
+			cnt += total / arr[i];
+			temp = total %  arr[i];
+			if (temp == 0) break;
+			total = temp;
+		}
+		System.out.println(cnt);
 
 	}
 
