a3c05fd6ac71787ecaed53e193b628163fdba2cf
sty635 <95391896+sty635@users.noreply.github.com>
2024-10-15 14:00:48 +0900

    Add files via upload

--- src/co/jp/chapter01/JavaHomework01.java
+++ src/co/jp/chapter01/JavaHomework01.java
@@ -1,10 +1,23 @@
 package co.jp.chapter01;
 
+import java.awt.TexturePaint;
+
 public class JavaHomework01 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		//Q1 以下変数nの値は︖
+//		int i = 10;
+//		int n = i%5;
+//		n=0
+//		
+//		Q2 右の式を解釈してください: a = a > 10 ? 11 : 0;
+//		aが10より大きい値かどうかを判断する．真の場合，aに11の値を与える；偽の場合，0の値を与える．
+//		
+//		Q3　変数値を⽐較する演算⼦は = or == ?
+//		==である．
+//		
+//		Q4　ブリアン（ boolean ）変数に設定可能の値は「?」「?」。
+//		[true] [false]
 	}
 
 }
