21442259c92b36198eb6bd8bb5b3f476d01e7b97
ojiwa <ojiwa@R9000P2023SHEN>
2024-10-19 11:23:54 +0900

    1019

--- src/co/jp/chapter03/JavaChapter03.java
+++ src/co/jp/chapter03/JavaChapter03.java
@@ -3,8 +3,75 @@ package co.jp.chapter03;
 public class JavaChapter03 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//swtich可以写的if也可以写，但是用if写的不一定可以用switch写出来（switch的局限性）
+		//If 文
+		int a = 10;
+		int b = 20;
+		if (a>b) {
+			System.out.println("a>b");
+		}
+		else if(a == b){ 
+			System.out.println("a==b");
+		}
+		else if (a < b) {
+			System.out.println("a<b");	
+		}
+		else if (a  == b-10) {
+			System.out.println("a==b-10");	//只要if条件判断为true，则停止处理，后续处理忽视
+		}
+		
+		
+		//Swtich文 分支（case）文，只能判断一个值而不是If一样的范围
+		int age = 2;
+		switch (age) {
+		case 30:
+			System.out.println("30true");	
+			break;//break属于swtich语法限制，意思为跳出当前分支，如果没有的话会继续执行
 
+		case 20:
+			System.out.println("20true");	
+			break;
+			case 1:
+			case 2:
+			case 3:
+				//复数条件可用这种写法，不然会输出好几遍
+				System.out.println("3岁以前");
+			break;
+			default://所有case都不匹配的情况转移到此，通常写在最后面，虽然不写也没问题，但一般都要写
+			System.out.println("false");	
+		}
+		
+		
+		//for文（循环语法）for (初始化变量; 循环继续的条件; 每次循环所产生的变化)
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i is "+ i);	
+		}
+		//  省略写法
+		int j = 0;
+		for( ; j < 10; j++) {
+			System.out.println("j is "+ j);	
+			}
+		
+		int k = 0;
+		for( ; k < 10; ) {
+			System.out.println("k is "+ k);	
+			k++;
+			}
+		
+		int l = 0;
+		for( ; ; ) {
+			if (l<10) {
+			System.out.println("l is "+ l);	
+			l++;
+			}
+			else {break;}//如果没有这个break，则コンソール右上角的红方块不会熄灭，程序不会停
+			}
+
+
+		
+		
+		
+		}
 	}
 
-}
+
--- src/co/jp/chapter03/JavaHomework03.java
+++ src/co/jp/chapter03/JavaHomework03.java
@@ -4,7 +4,81 @@ public class JavaHomework03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//質問１︓成績ランキング判断を作成する「90以上︓A, 80-90︓B, 70-80︓C, 60-70︓
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		// 以下コードを完成してください。
+		if(a>=90) {
+		 System.out.println("A");
+		} 
+		else if(a<90&&a>=80) {
+		 System.out.println("B");
+		} 
+		else if(a<80&&a>=70) {
+		 System.out.println("C");
+		} 
+		else if(a<70&&a>=60) {
+		 System.out.println("D");
+		} 
+		else {
+		 System.out.println("E");
+		}
+		
+		//質問２：⽇本のコインを１円、５円、１０円、１００円、５００円があります。３３４０円のコイン数をもとめください。
+        int total = 3340; 
 
+        int fiveHundredYen = 0;
+        int hundredYen = 0;
+        int tenYen = 0;
+        int fiveYen = 0;
+        int oneYen = 0;
+
+        // 500円コインの枚数
+        for (int i = 0; total >= 500; i++) {
+            fiveHundredYen++;
+            total -= 500;
+        }
+
+        // 100円コインの枚数
+        for (int i = 0; total >= 100; i++) {
+            hundredYen++;
+            total -= 100;
+        }
+
+        // 10円コインの枚数
+        for (int i = 0; total >= 10; i++) {
+            tenYen++;
+            total -= 10;
+        }
+
+        // 5円コインの枚数
+        for (int i = 0; total >= 5; i++) {
+            fiveYen++;
+            total -= 5;
+        }
+
+        // 1円コインの枚数
+        for (int i = 0; total >= 1; i++) {
+            oneYen++;
+            total -= 1;
+        }
+
+        // 結果の表示
+        System.out.println("500円コイン: " + fiveHundredYen + "枚");
+        System.out.println("100円コイン: " + hundredYen + "枚");
+        System.out.println("10円コイン: " + tenYen + "枚");
+        System.out.println("5円コイン: " + fiveYen + "枚");
+        System.out.println("1円コイン: " + oneYen + "枚");
+        
+        //出力結果：500円コイン: 6枚
+        //100円コイン: 3枚
+        //10円コイン: 4枚
+        //5円コイン: 0枚
+        //1円コイン: 0枚
+		
+		
 	}
 
+
 }
+
