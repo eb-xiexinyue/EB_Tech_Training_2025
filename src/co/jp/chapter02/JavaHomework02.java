acb4d0bf66cd46dc9deca9344ef3cd2ab7d6b2bb
dengguan <515728277@qq.com>
2024-10-21 01:33:55 +0900

    feat: homework2 and 3

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -4,7 +4,26 @@ public class JavaHomework02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		// 質問1
+		System.out.println("this is line1\tthis line2");
+		// 1行
+		
+		// 質問2
+		// equals
+		
+		// 質問3
+		// 2. 1.0F + ""
+		
+		// 質問4
+		// 1.
+		// 3.0
+		// 2.
+		// "1.02.0"
+		
+		// 質問5
+		// a
+		int a = 10;
+		System.out.println("a");
 	}
 
 }
--- src/co/jp/chapter03/JavaHomework03.java
+++ src/co/jp/chapter03/JavaHomework03.java
@@ -4,7 +4,39 @@ public class JavaHomework03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		// question 1
+		int a = 56;
+		System.out.println("質問1");
+		System.out.println("成績ランキング判断。入力値=" + a);
 
+        if (a >= 90) {
+        	System.out.println("A");
+        } else if (a >= 80) {
+        	System.out.println("B");
+        } else if (a >= 70) {
+        	System.out.println("C");
+        } else if (a >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("E");
+        }
+        
+        // question 2
+        int amount = 3340;
+        int[] coins = {500, 100, 10, 5, 1};
+        int[] count = new int[coins.length];
+
+        // 大きい額のコインから順に使用していく
+        for (int i = 0; i < coins.length; i++) {
+            count[i] = amount / coins[i]; // コインの枚数
+            amount %= coins[i]; // 残りの金額
+        }
+
+        // 結果を表示
+        System.out.println("質問2");
+        System.out.println("3340円を作るためのコインの枚数:");
+        for (int i = 0; i < coins.length; i++) {
+            System.out.println(coins[i] + "円: " + count[i] + "枚");
+        }
 	}
-
 }
