5a91657d5e71cbcbe04129353c7184ce2c6de576
chocoooolate <chocoooolate@localhost>
2024-10-21 23:43:38 +0900

    1015　宿題

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -4,7 +4,22 @@ public class JavaHomework02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問1 : 1. １⾏ 
+		
+		//質問2 : 2. equals
+		
+		//質問3 : 2. 1.0F + ""
+		
+		//質問4 : 
+		//1. 3.0
+		//2. "1.02.0"
+		 
+		//質問5 :
+			int a = 10; 
+			System.out.println("a"); 
+			//答え："a"
+			}
 
 	}
 
-}
+
--- src/co/jp/chapter03/JavaHomework03.java
+++ src/co/jp/chapter03/JavaHomework03.java
@@ -4,6 +4,43 @@ public class JavaHomework03 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//質問1
+		int a = 56;
+		System.out.println("成績ランキング判断。入力値=" + a);
+		if (a >= 90) {
+			System.out.println("A");
+		} else if (a < 90 && a >= 80) {
+			System.out.println("B");
+		} else if (a < 80 && a >= 70) {
+			System.out.println("C");
+		} else if (a < 70 && a >= 60) {
+			System.out.println("D");
+		} else if (a < 60) {
+			System.out.println("E");
+		}
+		//質問2
+		int total = 3340;
+		int[]coins = {500, 100, 10, 5, 1};
+		int[]coinCount = new int[coins.length];
+		
+		for(int i = 0; i<coins.length;i++) {
+			coinCount[i] = total / coins[i];
+			total %= coins[i];
+		}
+		
+		System.out.println("500円玉"+ coinCount[0]);
+		System.out.println("100円玉"+ coinCount[1]);
+		System.out.println("10円玉"+ coinCount[2]);
+		System.out.println("5円玉"+ coinCount[3]);
+		System.out.println("1円玉"+ coinCount[4]);
+		
+		//500円 = ６枚
+		//100円 = ３枚
+		//10円 = ４枚
+		//5円 = ０枚
+		//1円 = ０枚
+
+
 
 	}
 
