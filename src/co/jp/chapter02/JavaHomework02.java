489572abd9bb6156c2fe9fb2c0ec43fcb93cba2f
j2023 <j2023@192.168.1.29>
2024-10-24 15:35:33 +0900

    20241024

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -2,14 +2,17 @@ package co.jp.chapter02;
 
 public class JavaHomework02 {
 //2行
-//equals
+//==
 //String.valueOf("1.0F")
 //3.0,"1.02.0"
 //a
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-            int a = 10 ;
-            System.out.println("a");
+            String A = "a";
+            String B = "b";
+            if (A == B);
+            System.out.println(B);
 	}
 
 }
+
--- src/co/jp/chapter04/JavaHomework04.java
+++ src/co/jp/chapter04/JavaHomework04.java
@@ -4,7 +4,17 @@ public class JavaHomework04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				if(i==4 || j==4 || i==j || i+j==8) {
+					System.out.print(i+""+j+"\t");
+				}
+				else {
+					System.out.print("\t");
+				}
 
-	}
-
+			}
+			System.out.print("\n");
+		}
+}
 }
