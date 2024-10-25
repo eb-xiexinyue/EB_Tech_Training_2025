13619f92407bacab6063458b59cbca185a96af3b
j2023 <j2023@192.168.1.29>
2024-10-25 10:34:15 +0900

    202410251034

--- src/co/jp/chapter02/JavaHomework02.java
+++ src/co/jp/chapter02/JavaHomework02.java
@@ -3,7 +3,7 @@ package co.jp.chapter02;
 public class JavaHomework02 {
 //1行
 //equals。アドレスを含めて比較する場合は＝＝
-//String.valueOf("1.0F")
+//1.0F + ""
 //3.0,"1.02.0"
 //a
 	public static void main(String[] args) {
