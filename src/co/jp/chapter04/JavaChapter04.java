fca182be753d192c1682e90f6cea9cdc90d4cd08
12937 <12937@jiayi_zhao>
2024-11-04 18:41:20 +0900

    宿題

--- src/co/jp/chapter04/JavaChapter04.java
+++ src/co/jp/chapter04/JavaChapter04.java
@@ -1,10 +1,156 @@
 package co.jp.chapter04;
 
+import java.util.Arrays;
+
 public class JavaChapter04 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-	}
+//		int[] arr = new int[4];
+//		int[] arrWIthInitial = new int[] {1,2,3,4};
+//		int[] arrWIthInitial1 = {1,2,3,4};
+//		
+//		 System.out.println(Arrays.toString(arr));
+//	     System.out.println(Arrays.toString(arrWIthInitial));
+//	     System.out.println(Arrays.toString(arrWIthInitial1));
+//	     
+//	    
+//	     long[] arrLong = new long [4];
+//	     char[] arrChar = new char[4];
+//	     boolean[] arrBool = new boolean[4];
+//	     float[] arrFloat = new float[4];
+	     
+//	     String[] arrStr = new String[4];
+//	     Object[] arrObj = new Object[4];
+//	     
+//	     System.out.println("arrat with long value :"+ Arrays.toString(arrLong));
+//	     System.out.println("arrat with char value :"+  Arrays.toString(arrChar));
+//	     System.out.println("arrat with boolean value :"+ Arrays.toString (arrBool));
+//	     System.out.println("arrat with float value :"+ Arrays.toString (arrFloat));
+//	     
+//	     System.out.println("array with String :"+ Arrays.toString(arrStr));
+//	     System.out.println("array with Object :"+ Arrays.toString(arrObj);
+//	     
+	     
+//	     String[] arrStr = {"a","b","c","d","e"};
+//	     System.out.println(arrStr[0]);
+//	     System.out.println(arrStr[1]);
+//	     System.out.println(arrStr[2]);
+//	     System.out.println(arrStr[3]);
+//	     System.out.println(arrStr[4]);
+//	     
+//
+//	     
+//	     for(int i=0;i<arrStr.length;i++)  {
+//	    	 System.out.println(arrStr[i]);
+//	    	 	
+//	     
+//		for(String s:arrStr) {
+//			System.out.println("迭代"+s);
+		
+//	}
+//	     String[][] array2D = {
+//	             {"a", "b"},
+//	             {"c", "d", "e"}
+//	         };
+//
+//	         System.out.println("length of array2D is " + array2D.length);
+//	         System.out.println(Arrays.toString(array2D[1]));
+//	         System.out.println(array2D[1][0]);
+//	         
+//	         for(String[] outS : array2D) {
+//	 			for(String inS : outS) {
+//	 				System.out.println(inS);
+//	 			}
+//	         }
+//	 		
+//		String[][][] array3D = new String[3][2][2];
+//		String[][][] array3DwithInitial = new String[][][] {
+//			{},{{},{}},{{}}
+//		};
+//		
+	 		
+	       
+//	     String[] arr1 = {"str1","str2","str3"};
+//	     
+//	     arr1[2] = "new str3";
+//	     
+//	     System.out.println(arr1[2]);
+//	     
+//	     String[][] arr2D = {{"s11","s12","s13"},{"s21","s22","s23"}};
+//	     arr2D[1][1] = "ns22";
+//	     
+//	     System.out.println(arr2D[1][1]);
+//	     System.out.println(Arrays.toString(arr2D[1]));
+		
+		int[] arr = {1,2,3};
+		
+		int[] arr1 = new int[4];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr1[i] = arr[i];
+			
+		}
+		
+		arr1[3] = 4;
+		System.out.println(Arrays.toString(arr1));
+		
+		
+		//数数组长度不可变，只能更改已有数组的值
+	     
+		int[] arr2 = new int[2];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = arr[i];
+        }
+        System.out.println("Arr2 (manual copy): " + Arrays.toString(arr2));
 
+        System.out.println( Arrays.toString(Arrays.copyOf(arr, 4)));
+
+        // 使用 Arrays.copyOf 方法来复制 arr 的前2个元素
+        System.out.println( Arrays.toString(Arrays.copyOf(arr, 2)));
+   
+	 
+        int[] newArr = Arrays.copyOf(arr, 4);
+        System
+        
+        
+        最后一问：二元数组累加累乘
+        
+	
+	
+	}
+	    
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	       
+	}
+	
+	
 }
+		 
+	         
+	         
+	         
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		
+	
+
+
--- src/co/jp/chapter04/JavaHomework04.java
+++ src/co/jp/chapter04/JavaHomework04.java
@@ -5,6 +5,90 @@ public class JavaHomework04 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		
+	//問題１:int[] array = new int[4]; 各要素の値を記載してください。
+		
+	//	0,0,0,0 
+	
+	//問題２:int[] array = new int[] {5, 6}; 各要素の値を記載してください。
+		
+	//	5.6
+		
+	//問題３:int[][] array = new int[2][3]; 各要素の値を記載してください。
+		
+	//	0,0,0
+	//	0,0,0
+		
+	//問題4：
+//		int[][] array2d = new int[4][2];
+//		for (int[] out : array2d) {
+//		    for (int inner : out) {
+//		        System.out.println(inner);
+//		    }
+//		}
+//		4行2列の配列、全ての要素が0
+		
+		
+	//	問題5: int[] array = new int[] {1, 3, 5, 7, 100, 0, 1}; の最大値と最小値を求める。	
+//		int[] array = {1, 3, 5, 7, 100, 0, 1};
+//		int max = array[0];
+//		int min = array[0];
+//
+//		for (int num : array) {
+//		    if (num > max) max = num;
+//		    if (num < min) min = num;
+//		}
+//
+//		System.out.println("最大值: " + max);
+//		System.out.println("最小值: " + min);
+//
+//		最大值: 100
+//		最小值: 0
+		
+		//問題6: 問題1の配列 array のSUM（合計値）を求める
+//		int[] array = new int[4];
+//		int sum = 0;
+//		for (int num : array) {sum += num;}
+//		
+//		System.out.println(sum);
+//		0
+
+		//問題7: 行列（2次元配列）の和と積を計算してください。
+		
+		int[][] array = {
+			    {1, 2, 3},
+			    {4, 5, 6}
+			};
+
+			int sum = 0;
+			int product = 1;
+			
+           //方法①
+
+//			for (int i = 0; i < array.length; i++) { 
+//			for (int j = 0; j < array[i].length; j++) { 
+//			
+//			sum += array[i][j]; 
+//			product *= array[i][j];
+//			    }
+//			}
+//
+//			System.out.println(sum);
+//			System.out.println(product);
+
+			//方法②
+			
+			for (int[] out : array) {
+			    for (int inner : out) {
+			        sum += inner;      
+			        product *= inner;  
+			      
+			    }
+			}
+		
+			System.out.println(sum);
+			System.out.println(product);
+		
 	}
 
 }
