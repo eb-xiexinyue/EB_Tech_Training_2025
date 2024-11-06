279b44c24bcfb108a6d2e79db9290e2c15423cc2
ojiwa <ojiwa@R9000P2023SHEN>
2024-11-06 23:16:07 +0900

    241106

--- src/co/jp/chapter06/JavaChapter06.java
+++ src/co/jp/chapter06/JavaChapter06.java
@@ -4,7 +4,66 @@ public class JavaChapter06 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		//public static 修饰子
+		//void：返却值 
+		//main：函数名字
+		//(String[](引数の型) args（引数名）) {
+		System.out.println(args);
+		//不是void的情况需要return
+		System.out.println(sum(1,2));
+		System.out.println(" static String combine(String str1,String str2) の戻り値"+combine("aaa","bbb" ));
+		sumOverload();
+		System.out.println("sumOverload(1,3):"+sumOverload(1,3));
+		sumOverload("11","22");
+		sumOverload(1.0f,2.3f);
+		 System.out.println(fact(3));
 
 	}
+	//void sum改成int
+	static int sum(int a, int b) {
 
+		return a+b;
+		//在retune后面写其他代码，会变成dead code，因为已经结束了
+	
+	}
+ static String combine(String str1,String str2) {
+	String res= str1 + str2;
+	return res;
+ }
+ static void testVoid(String str) {
+		System.out.println(str);
+		return;
+ }
+ //オーバーロード検証
+ static void sumOverload() {
+	 System.out.println("sumOverload:");
+ }
+ 
+ static int sumOverload(int a,int b) {
+	 System.out.println("sumOverload(int a,int b):"+(a+b));
+	 return a +b;
+ }
+ 
+ static void sumOverload(String a,String b) {
+	 System.out.println("sumOverload(String a,String b):"+(Integer.valueOf(a)+Integer.valueOf(b)));
+ }
+
+ static float sumOverload(float a,float b) {
+	 System.out.println("sumOverload(float a,float b):"+(a+b));
+	 return a+b;
+ }
+ 
+ //再帰
+ //再帰は普通Forと比べてあまり便利でないため、あまり使わない
+ static int fact(int n) {
+	 if (n == 0) return 1; /* 脱出条件。0! は 1 である ,{}は省略している*/
+	 else return fact(n - 1) * n; /* n! は (n-1)! に n を乗じたもの。再帰呼出し */
+	}
+
+ 
+ 
+ 
+ 
+ 
+ 
 }
--- src/co/jp/chapter06/JavaHomework06.java
+++ src/co/jp/chapter06/JavaHomework06.java
@@ -3,8 +3,66 @@ package co.jp.chapter06;
 public class JavaHomework06 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 質問１．以下関数の修飾⼦、返却型、関数名、引数（引数の型・名称）を回答してください。
+		//public static void main(String[] args);
+		//修飾⼦:public static 返却型void、関数名main、引数（引数の型String[] ・名称args）
+		//質問２．配列の和、MAX値、MIN値を求める処理を関数化してください、main関数に呼び出してください。
+		int[] datas= {1,2,3,4,5,6,7};
+		int total = total(datas);
+		int max = max(datas);
+		int min = min(datas);
+		System.out.println(total);
+		System.out.println(max);
+		System.out.println(min);
+		//質問３以下メソッドの実施結果を教えてください。:		null
+//		public class Kicker {
+//			 private static void sayHello(String y) {
+//			 y = "Hello world";
+//			 }
+//			 public static void main(String args[]) {
+//			 String x = null;
+//			 sayHello(x);
+//			 System.out.println(x);
+//			 }
+//			}
 
+		//質問４︓以下メソッドの実施結果を教えてください。:value2
+//		public class Kicker {
+//			 private static void fillArray(String[] array) {
+//			 array[0] = "value2";
+//			 }
+//			 public static void main(String args[]) {
+//			 String[] array = new String[]{"value1"};
+//			 fillArray(array);
+//			 System.out.println(array[0]);
+//			 }
+//			}
+		//質問５︓以下メソッドの実施結果を教えてください。
+		
+		
+		
+		
 	}
+	
+	
+ static int total(int[] datas) {
+int total = 0;
+	 for(int num:datas) {total += num; }
+	return total;
+ }
+ 
+ static int max(int[] datas) {
+     int max = 0;
 
+	 for(int num:datas) {if ( num>max) {max= num;}}
+	return max;
+ }
+ 
+ static int min(int[] datas) {
+     int min = 0;
+
+	 for(int num:datas) {if ( num<min) {min= num;}}
+	return min;
+ }
+ 
 }
