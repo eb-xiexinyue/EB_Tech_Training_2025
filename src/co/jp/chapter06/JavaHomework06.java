c14fa733b5cd7697d0f40d1f5ef3307d42e40bba
12937 <12937@jiayi_zhao>
2024-11-12 16:15:40 +0900

    1111

--- src/co/jp/chapter06/JavaHomework06.java
+++ src/co/jp/chapter06/JavaHomework06.java
@@ -105,69 +105,132 @@ public class JavaHomework06 {
 //		    }
 //		}
 //		
-//		true
+//		結果:true
 		
 		//質問6
-		public static void printArray(Object[] objs) {
-		    if(objs == null) {
-		        System.out.println("null");
-		        return;
-		    }
-
-		    String prefix = "";
-		    StringBuilder sb = new StringBuilder();
-		    sb.append("[");
-		    for(Object obj : objs) {
-		        sb.append(prefix);
-		        sb.append(String.valueOf(obj));
-		        prefix = ", ";
-		    }
-		    sb.append("]");
-		    System.out.println(sb.toString());
-		}
-	}
+//		public static void printArray(Object[] objs) {
+//		    if(objs == null) {
+//		        System.out.println("null");
+//		        return;
+//		    }
+//
+//		    String prefix = "";
+//		    StringBuilder sb = new StringBuilder();
+//		    sb.append("[");
+//		    for(Object obj : objs) {
+//		        sb.append(prefix);
+//		        sb.append(String.valueOf(obj));
+//		        prefix = ", ";
+//		    }
+//		    sb.append("]");
+//		    System.out.println(sb.toString());
+//		}
+//	}
 	   //6-1
-	    public static int[] mergeArray(int[] left, int[] right) {
-	        int[] result = new int[left.length + right.length];
-	        System.arraycopy(left, 0, result, 0, left.length);
-	        System.arraycopy(right, 0, result, left.length, right.length);
-	        return result;
-	}
+//	    public static int[] mergeArray(int[] left, int[] right) {
+//	        int[] result = new int[left.length + right.length];
+//	        System.arraycopy(left, 0, result, 0, left.length);
+//	        System.arraycopy(right, 0, result, left.length, right.length);
+//	        return result;
+//	}
 
+	   //6-2
+//		static int[] subArray(int[] array, int startIndex, int length) {
+//		    int[] subArray = new int[length];
+//		    System.arraycopy(array, startIndex, subArray, 0, length);
+//		    return subArray;
+//		}
+
+	    //質問７︓以下静的なメソッドを定義してください。
+//		1. メソッド名︓trim
+//		2. 引数１︓charの配列
+//		3. 戻り値︓charの配列
+//		4. 処理内容︓引数１のcharの配列先頭と末尾の空⽩⽂字列を削除してください
+		
+//	    static char[] trim(char[] array) {
+//	        int start = 0;
+//
+//	        while (start < array.length && array[start] == ' ') {
+//	            start++;
+//	        }
+//
+//	        int end = array.length - 1;
+//
+//	      
+//	        while (end >= 0 && array[end] == ' ') {
+//	            end--;
+//	        }
+//
+//	       
+//	        if (start > end) {
+//	            return new char[0];
+//	        }
+//
+//	  
+//	        char[] output = new char[end - start + 1];
+//	        System.arraycopy(array, start, output, 0, output.length);
+//
+//	        return output;
+//	    }
+//}
+
+	 //質問８︓バブルソート⽤メソッドを作成してください。
+//		1. メソッド名︓bubbleSort
+//		2. 引数１︓intの配列
+//		3. 戻り値︓ソート済みの配列
+//		4. 処理内容︓bubbleSortアルゴリズムを⽤い、⼊⼒した引数の配列をソートしてください。
+       
+//	    public static void bubbleSort(int[] array) {
+//   
+//            for (int i = 0; i < array.length - 1; i++) {
+//       
+//            for (int j = 0; j < array.length - 1 - i; j++) {
+//                 
+//            	if (array[j] > array[j + 1]) {
+//               
+//                int temp = array[j];
+//                array[j] = array[j + 1];
+//                array[j + 1] = temp;
+//            }
+//        }
+		
+		
+		//質問９︓メソッドの呼び出す練習
+		public class Main {
+		    public static void main(String[] args) {
+		        int[] sortedArray = bubbleSort(mergeArray(new int[]{1, 3, 5}, new int[]{2, 4, 6}));
+		        for (int num : sortedArray) {
+		            System.out.print(num + " ");
+		        }
+		    }
+
+		    public static int[] mergeArray(int[] left, int[] right) {
+		        int[] result = new int[left.length + right.length];
+		        System.arraycopy(left, 0, result, 0, left.length);
+		        System.arraycopy(right, 0, result, left.length, right.length);
+		        return result;
+		    }
+
+		    public static int[] bubbleSort(int[] array) {
+		        for (int i = 0; i < array.length - 1; i++) {
+		            for (int j = 0; j < array.length - 1 - i; j++) {
+		                if (array[j] > array[j + 1]) {
+		                    int temp = array[j];
+		                    array[j] = array[j + 1];
+		                    array[j + 1] = temp;
+		                }
+		            }
+		        }
+		        return array;
+		    }
+		}
+
+            
 	   
-	    //7
-	    static char[] trim(char[] array) {
-	        int start = 0;
-
-	        while (start < array.length && array[start] == ' ') {
-	            start++;
-	        }
-
-	        int end = array.length - 1;
-
-	      
-	        while (end >= 0 && array[end] == ' ') {
-	            end--;
-	        }
-
-	       
-	        if (start > end) {
-	            return new char[0];
-	        }
-
-	  
-	        char[] output = new char[end - start + 1];
-	        System.arraycopy(array, start, output, 0, output.length);
-
-	        return output;
-	    }
-}
 	
 	
 	
-	
-	}
-}
+
 		
 	
 
