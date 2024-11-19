453d4741f83d9d3c66b5fdb8b0b2679b5cc49a53
xie <xie@192.168.1.3>
2024-11-19 21:40:49 +0900

    20241119 見本ソース更新

--- src/co/jp/chapter08/JavaChapter08.java
+++ src/co/jp/chapter08/JavaChapter08.java
@@ -4,7 +4,44 @@ public class JavaChapter08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Child child = new Child(20, 77);
+		
+//		System.out.println(child.familyName);
+//		//System.out.println(child.givenName); //アクセス不可
+//		System.out.println(child.money);
+//		System.out.println(child.age);
+//		
+//		child.init();
+		
+//		Son son = new Son(5);
+		
+//		System.out.println(son instanceof Son);
+//		System.out.println(son instanceof Child);
+//		System.out.println(son instanceof Parent);
+		
+//		System.out.println(child.getMoney());
+//		System.out.println(child.getParentMoney());
+//		String strFromChild = child.toString();
+//		
+//		System.out.println(strFromChild);
+//		System.out.println(child);
+		Child target1 = new Child(35, 77);
+		
+		Child target2 = new Child(35, 78);
+		
+		System.out.println(child);
+		System.out.println(target1);
+		System.out.println(target2);
+		
+		System.out.println(child.equals(target1));
+		System.out.println(child.equals(target2));
+		
+		Object str = new String("DCNET"); 
+		String value1 = (String)str; 
+		String value2 = String.class.cast(value1); 
+		
+		System.out.println(value1);
+		System.out.println(value2);		 
 	}
 
 }
