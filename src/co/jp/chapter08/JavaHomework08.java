777194fd107a966f6a6d40d965a360f696d7228e
gendogo <whj.bnm@qq.com>
2024-11-19 20:37:52 +0800

    宿題8の回答です

--- src/co/jp/chapter08/JavaHomework08.java
+++ src/co/jp/chapter08/JavaHomework08.java
@@ -3,7 +3,18 @@ package co.jp.chapter08;
 public class JavaHomework08 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		/*問題１: true 変数aとbは同じオブジェクトを指す
+		 * 		true 値は同じ
+		 * 		NullPointerException
+		 */
+		
+		/*問題2: false メモリアドレスが違ってる。
+		 * 		true　入力値が同じであれば、trueを返す。
+		 
+		 */
+		
+		//問題3: 16行目のprintメソッドの引数タイプをParentに変更する。
+		
 
 	}
 
