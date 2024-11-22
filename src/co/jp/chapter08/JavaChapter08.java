732577b1d956551c6f5b31a31cb7ffa710be4dc9
ojiwa <ojiwa@R9000P2023SHEN>
2024-11-22 16:18:08 +0900

    1122

--- src/co/jp/chapter08/JavaChapter08.java
+++ src/co/jp/chapter08/JavaChapter08.java
@@ -3,8 +3,26 @@ package co.jp.chapter08;
 public class JavaChapter08 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+		Child child = new Child(20);
+		System.out.println("age："+child.age);
+		System.out.println("familyname:"+child.familyName);
+		System.out.println("money："+child.money);
+		
+		child.init();
+		//child.init(5); 如果里面有数字那就是要用到public void init(int x) 
+		Son son = new Son(5);
+		
+		//is a 关系递进 instanceof：型判断
+		System.out.println(son instanceof Child);
+		System.out.println(son instanceof Parent);
+		
+		System.out.println(child.getMoney());
+		System.out.println(child.getParentMoney());
+		System.out.println(child);//如果没有复写，会打印地址
+		String strFromChild=child.toString();
+		System.out.println(strFromChild);//调用tostring函数的返回值
+		child.equals(strFromChild);
 	}
 
 }
