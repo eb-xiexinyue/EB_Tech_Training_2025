e9982bbd15675389771d3eb50f09af93861a65c0
12937 <12937@jiayi_zhao>
2024-11-25 21:41:51 +0900

    1125

--- src/co/jp/chapter08/JavaChapter08.java
+++ src/co/jp/chapter08/JavaChapter08.java
@@ -4,6 +4,44 @@ public class JavaChapter08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		Child child =new Child(20);
+		
+//		System.out.println(child.familyName);
+//		System.out.println(child.givenName);//アクセス不可
+//		System.out.println(child.money);
+//		System.out.println(child.age);
+//		
+//		child.init();
+//		child.init(5);
+		
+		
+//		Son son = new Son(5);
+		
+//		System.out.println(son instanceof Son);
+//		System.out.println(son instanceof Child);
+//		System.out.println(son instanceof Parent);
+		
+		
+//        System.out.println(child.getMoney());
+//        System.out.println(child.getParentMoney());
+		
+//		String strFromChild = child.toString();
+//		
+//		System.out.println(child);
+//		System.out.println(strFromChild);
+		Child target1 = new Child(35,77);
+		
+		Child target2 = new Child(35,78);
+		
+		System.out.println(child);
+		System.out.println(target1);
+		System.out.println(target2);
+		
+		System.out.println(child.equals(target1));
+		System.out.println(child.equals(target2));
+		
+		
+        
 
 	}
 
--- src/co/jp/chapter08/JavaHomework08.java
+++ src/co/jp/chapter08/JavaHomework08.java
@@ -4,6 +4,24 @@ public class JavaHomework08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		
+		
+//	質問１
+//		String x = "a";
+//		String y = "a";
+//		System.out.println(x == y);      // true? false? 原因は？
+//		
+//		true　x と y はどちらも同じリテラル "a" を指しているため、== 演算子で比較すると true になります。
+//		
+//		System.out.println(x.equals(y)); // true? false? 原因は？
+//		
+//		true　equals() メソッドは文字列の値を比較します。x と y はどちらも "a" という同じ文字列の内容を持つため、結果は true になります。
+//		
+		String empty = "";
+		String nullString = null;
+		System.out.println(nullString.equals(empty)); // true? false? 原因は？
+
+
 
 	}
 
