719576275d07d70ee48008c09003df072d89307d
kakuclaire <kakuclaire743@gmail.com>
2025-01-19 19:19:20 +0900

    Update JavaHomework09.java

--- src/co/jp/chapter09/JavaHomework09.java
+++ src/co/jp/chapter09/JavaHomework09.java
@@ -1,1 +1,57 @@
 
+package co.jp.chapter09;
+
+public class JavaHomework09 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+
+//	質問１︓以下ソースにラムダ式の使う場所を解釈してください。
+	
+//	import java.awt.event.ActionEvent;
+//	import java.awt.event.ActionListener;
+//	import javax.swing.JButton;
+//	import javax.swing.JFrame;
+//	public class GUISample {
+//	public static void main(String[] args) {
+//	JFrame window = new JFrame("DCNet Java 教育");
+//	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	window.setSize(800, 600);
+//	JButton btn = new JButton("hello world");
+//　　 window.getContentPane().add(btn);
+//	btn.addActionListener((ActionEvent e) -> {
+//	System.out.println("ボタンクリックしました。");
+//	 });
+//
+//	  window.setVisible(true);
+//	  }
+//	 このラムダ式は匿名内部クラスを作成して ActionListener インターフェースを実装することです。
+//	　　匿名内部クラスを置き換えた形で動作し、クリックイベントを処理し。「ボタンクリックしました」を出力します。
+		
+
+	//質問２︓以下ソースに匿名クラスの利⽤する⾏は︖
+
+
+//	public class Kicker {
+//	 public static void main(String... args) {
+//	 new Thread() {
+//	 @Override
+//	 public void run() {
+//	 System.out.println("thread running...");
+//	 }
+//	 }.start();
+//	 }
+//	}		
+
+//答えは以下の部分です。
+//	new Thread() {
+//	    @Override
+//	    public void run() {
+//	        System.out.println("thread running...");
+
+
+
+
+
