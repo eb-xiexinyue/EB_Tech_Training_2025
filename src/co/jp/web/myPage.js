6aa013b7a91b31ba0715ed8b169a74cbaddc457c
ojiwa <ojiwa@R9000P2023SHEN>
2025-01-20 15:44:14 +0900

    0120

---
+++ src/co/jp/web/myPage.js
@@ -0,0 +1,7 @@
+
+document.getElementById('colorChangeButton').addEventListener('click', () => {
+    // 背景色　変更
+    document.body.style.backgroundColor = '#333';
+    // 文字色　赤色
+    document.body.style.color = '#ff0000';
+});
